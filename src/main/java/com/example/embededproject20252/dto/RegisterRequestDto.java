6e1068ad735d1e9e1114f03506400330087cd9ef
sg.chan <163969084+kchan1057@users.noreply.github.com>
2025-09-24 14:28:20 +0900

    Update RegisterRequestDto.java

--- src/main/java/com/example/embededproject20252/dto/RegisterRequestDto.java
+++ src/main/java/com/example/embededproject20252/dto/RegisterRequestDto.java
@@ -12,8 +12,6 @@ public record RegisterRequestDto(
     String name,
 
     @NotBlank(message = "전화번호는 필수입니다.")
-    @Pattern(regexp = "^01[0-9]-?[0-9]{4}-?[0-9]{4}$",
-        message = "올바른 전화번호 형식이 아닙니다.")
     String phoneNumber
 ) {
 }
